70c95b546ce9f104eee519aad1aafca9230d6054
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-02-22 16:08:08 +0100

    Added MongoDB.Bson to items

--- src/Play.Catalog.Service/Entities/Items.cs
+++ src/Play.Catalog.Service/Entities/Items.cs
@@ -1,7 +1,14 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Play.Catalog.Service.Entities
 {
     public class Items
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)] // Converts Guid to a string in MongoDB
+
         public Guid Id { get; set; }
 
         public required string Name { get; set; }

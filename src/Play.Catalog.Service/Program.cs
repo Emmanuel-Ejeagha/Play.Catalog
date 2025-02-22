1da06f88755c5a446152dfab68f0ace3ecbcf310
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-02-22 16:06:50 +0100

    Added Configuration to Program file

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers(Options =>
+    {
+        Options.SuppressAsyncSuffixInActionNames = false;
+    });
 
 var app = builder.Build();
 

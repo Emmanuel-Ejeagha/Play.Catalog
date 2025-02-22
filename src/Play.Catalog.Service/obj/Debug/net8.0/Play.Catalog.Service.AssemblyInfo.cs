5ea6a910e96e2cc5dc93b1128be1908197363a84
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-02-22 04:52:07 +0100

    Add: modified Dto

--- src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
+++ src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29aa156db5caf1d78c479618d4f2b9b61cef7ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d007b489a918ac8a0bf459ef5afe86d01076007")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

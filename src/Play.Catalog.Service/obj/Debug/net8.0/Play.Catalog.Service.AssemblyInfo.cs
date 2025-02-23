eef702677452d7230da5662e1fc7fbe1ff5ca6bc
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-02-23 11:19:14 +0100

    Added MongoDB settings to Program file

--- src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
+++ src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c277255fdef8d0247e3396f4dc82cd7f194e698b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50520ba436f00f1494e988aa3ed6bd7cd3f731de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
